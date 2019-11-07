265ae98d5f845d5d7d5576124236b56d662f6a0b
Jan Alexander <janalexand@gmail.com>
2019-11-07 19:47:03 +0100

    joined two .R files

--- HW3_G15_JAL.R
+++ HW3_G15_JAL.R
@@ -1,6 +1,12 @@
 # HOMEWORK 3
 # Group nr 15
-
+#
+#
+library(coin)
+library(ggplot2)
+library(reshape2)
+library(rmutil)
+#
 # FUNCTION: calc.median.diff: --------------
 # Function: calculate the difference in median between two groups.
 #
@@ -13,7 +19,7 @@ calc.median.diff <- function(ind, vec){
   group2 <- vec[!(1:length(vec)) %in% ind]
   # calculate the difference in means
   return(median(group1) - median(group2))
-  }
+}
 
 # FUNCTION: median.test: --------------
 # Function: calculate the p-value of the median difference between
@@ -27,7 +33,7 @@ calc.median.diff <- function(ind, vec){
 # param: y = vector values group 2
 #
 median.test <- function(x, y){
-  N <- 15000
+  N <- 10000
   realization <- median(x) - median(y)
   len_x <- length(x)
   len_y <- length(y)
@@ -46,74 +52,92 @@ median.test <- function(x, y){
                              calc.median.diff(
                                sample(c(1:len_vec),len_x), 
                                vec)
-                             )
+    )
   }
   if(FALSE){
     hist(abs(median.diff), main = paste('p = ', 
-                                   mean(abs(realization) <= abs(median.diff))))
+                                        mean(abs(realization) <= abs(median.diff))))
     abline(v = abs(realization), col = "red",
            lty = 1, lwd = 2)
-    }
+  }
   # The distribution will be symmetrical. The p-value can be calculated by taking
   # the absolute value.
   return(mean(abs(realization) <= abs(median.diff)))
 }
 
-calculate.power <- function(delta, d, dist_arg, N_tests = 150 ,p_value = 0.05){
+# FUNCTION: calculate.power: --------------
+# Function: calculate the proportion of tests that yield a p-value
+# smaller than 0.05 for the median.test, the Wilcoxon exact
+# test and the permutation t-test after repetitive sampling.
+# 
+# param: delta = effect size
+# param: d = random distribution function
+# dist_arg = arguments or the distribution function
+# N_tests = the amount of repetitions
+# p_value = significance level
+#
+calculate.power <- function(delta, d, dist_arg, N_tests = 150, p_value = 0.05){
   cat('power calculation for distribution : ')
-  cat(d) 
+  cat(d)
   cat('\n')
-  p.perm <- p.wmw <- c()
+  p.perm <- p.wmw <- p.t <- c()
   for(i in 1:N_tests){
     Y1 <- do.call(what = d, 
                   args = dist_arg)
     Y2 <- do.call(what = d, 
                   args = dist_arg) + delta
+    df <- data.frame(rep(c('A', 'B'), each = n), c(Y1, Y2))
+    colnames(df) <- c("group", "Y")
     p.perm[i] <- median.test(x = Y1, y = Y2)
     p.wmw[i] <- wilcox.test(Y1, Y2, exact = TRUE)$p.value
+    p.t[i] <- pvalue(oneway_test(Y~group, data = df, 
+                                 distribution = approximate(nresample = 10000)))
   }
   if(FALSE){
     hist(p.perm)
     hist(p.wmw)
   }
   return(c(mean(p.perm < p_value),
-           mean(p.wmw < p_value)
-           )
-         )
+           mean(p.wmw < p_value),
+           mean(p.t < p_value)
+  )
+  )
 }
 
-library(ggplot2)
-library(reshape2)
+# Simulations
+n <- 20
+distributions <- c('rt', 'rt', 'rexp', 'rlogis', 'rnorm', 'runif', 'rlaplace')
+dist_names <- c('t3', 't5', 'exp', 'logis', 'norm', 'unif', 'laplace')
+dist_args <- list(list(n, 3), list(n, 5), list(n), list(n), list(n), list(n), list(n))
 
-n <- 40
-distributions <- c('rt', 'rt', 'rexp', 'rlogis', 'rnorm', 'runif')
-dist_names <- c('t3', 't5', 'exp', 'logis', 'norm', 'unif')
-dist_args <- list( list(n, 3), list(n, 5), list(n), list(n), list(n), list(n))
+power.perm <- power.wmw <- power.t <- c()
 
-power.perm <- c()
-power.wmw <- c()
-
-for(delta in c(0, 0.01, 0.05, 0.1, 0.25)){
-  for(i in 1:6){
+for(delta in c(0, 0.5, 0.75)){
+  for(i in 1:7){
+    N_tests <- 1000
     d <- distributions[i]
-    res <- calculate.power(delta, d, dist_args[[i]], N_tests = 25)
+    res <- calculate.power(delta, d, dist_args[[i]], N_tests = N_tests)
     power.perm[i] <- res[1]
     power.wmw[i] <- res[2]
+    power.t[i] <- res[3]
   }
-  
   df_power <- data.frame(Distribution = dist_names,
-                         Perm = rep(x = 0.0, 6),
-                         wmw = rep(x = 0.0, 6))
+                         Perm = rep(x = 0.0, length(distributions)),
+                         wmw = rep(x = 0.0, length(distributions)),
+                         t = rep(x = 0.0, length(distributions)))
   df_power$Perm <- power.perm
   df_power$wmw <- power.wmw
+  df_power$t <- power.t
   df_power <- melt(df_power, id.vars = 'Distribution')
   
   write.csv(df_power, paste('delta_', delta*100, 'percent.csv', sep=''))
   
   if(delta != 0){
-    title <- paste('Power of perm vs wmw for different distributions for delta = ', delta)
+    title <- paste('Power of perm vs wmw for different distributions for delta = ', delta,
+                   '\n(n =', n, ', # simulations =', N_tests, ')')
   }else{
-    title <- paste('Proportion of type I error - delta = ', delta)
+    title <- paste('Proportion of type I error - delta = ', delta,
+                   '\n(n =', n, ', # simulations =', N_tests, ')')
   }
   
   plot1 <- ggplot(data = df_power, aes(x = Distribution, y = value, fill = variable)) + 
@@ -123,7 +147,6 @@ for(delta in c(0, 0.01, 0.05, 0.1, 0.25)){
     ylab('Power') + 
     coord_flip()
 
-  
   ggsave( paste('delta', delta*100, '.png', sep = ''), 
           plot = plot1, 
           device = 'png'
