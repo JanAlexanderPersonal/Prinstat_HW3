57b73a857dae4762687b1920df9f43f11afa70f6
JanAlexanderPersonal <48419030+JanAlexanderPersonal@users.noreply.github.com>
2019-11-10 11:53:29 +0100

    Alternatief voor delta
    Op deze manier is er een constante delta tussen de populaties Y1 en Y2

--- HW3_G15.R
+++ HW3_G15.R
@@ -26,7 +26,7 @@ calc.median.diff <- function(ind, vec){
 # vector x and vector y, based on the null hypothesis F1(x) = F2(x)
 #
 # When the number of combinations is sufficiently small to do a full 
-# permutation test (limit at 15000), the full permutation test will
+# permutation test (limit at 10000), the full permutation test will
 # be performed. 
 #
 # param: x = vector values for group 1
@@ -39,49 +39,31 @@ median.test <- function(x, y){
   len_y <- length(y)
   vec <- c(x, y)
   len_vec <- len_x + len_y
-  if(choose(n = len_x+len_y, len_x) < N){
-    #cat('A limited number of combinations: full permutation test')
+  if(choose(n = len_vec, len_x) < N){
+    # A limited number of combinations: full permutation test 
     median.diff <- combn(len_vec, 
                          len_x, 
                          function(ind){
                            return(calc.median.diff(ind, vec))
                          })
   } else {
-    #cat('Too many combinations - A sample test will be performed.\n')
+    # Too many combinations - A sample test will be performed.
     median.diff <- replicate(N, 
                              calc.median.diff(
                                sample(c(1:len_vec),len_x), 
                                vec)
     )
   }
-  if(FALSE){
-    hist(abs(median.diff), main = paste('p = ', 
-                                        mean(abs(realization) <= abs(median.diff))))
-    abline(v = abs(realization), col = "red",
-           lty = 1, lwd = 2)
-  }
   # The distribution will be symmetrical. The p-value can be calculated by taking
   # the absolute value.
   return(mean(abs(realization) <= abs(median.diff)))
 }
 
-# Simulations
-n <- 20         # sample size
-N <- 200        # test simulations
-distributions <- c('rt', 'rt', 'rexp', 'rlogis', 'rnorm', 'runif', 'rlaplace')
-dist_names <- c('t3', 't5', 'exp', 'logis', 'norm', 'unif', 'laplace')
-dist_args <- list(list(n, 3), list(n, 5), list(n), list(n), list(n), list(n), list(n))
-
-power.med <- power.wmw <- power.t <- c()
-
-# For simulations under H0 (delta = 0) and Ha (delta = 1)
-for(delta in 0:1) {
-  if(delta) {
-    title <- paste0('Power_', n, '_', N) }
-  else{
-    title <- paste0('TypeI_error_', n, '_', N) }
-  
-  # for different distributions
+calc.rejection <- function(N, n, delta, p.val = 0.05){
+  distributions <- c('rt', 'rt', 'rexp', 'rlogis', 'rnorm', 'runif', 'rlaplace')
+  dist_names <- c('t3', 't5', 'exp', 'logis', 'norm', 'unif', 'laplace')
+  dist_var <- c(3, 5/3, 1, 2*pi^2/6, 1, 1/12, 2)
+  dist_args <- list(list(n, 3), list(n, 5), list(n), list(n), list(n), list(n), list(n))
   for(i in 1:length(distributions)) {
     cat(paste('power calculation for distribution : ',
               distributions[i], '\n'))
@@ -92,7 +74,7 @@ for(delta in 0:1) {
       Y1 <- do.call(what = distributions[i], 
                     args = dist_args[[i]])
       Y2 <- do.call(what = distributions[i], 
-                    args = dist_args[[i]]) + delta * sd(Y1)/2
+                    args = dist_args[[i]]) + delta * dist_var[i] / 2
       df <- data.frame(rep(c('A', 'B'), each = n), c(Y1, Y2))
       colnames(df) <- c("group", "Y")
       p.med[j] <- median.test(x = Y1, y = Y2)
@@ -101,9 +83,9 @@ for(delta in 0:1) {
                                    distribution = approximate(nresample = 10000)))
     }
     # calculate the proportion of H0 rejections
-    power.med[i] <- mean(p.med < 0.05)
-    power.wmw[i] <- mean(p.wmw < 0.05)
-    power.t[i] <- mean(p.t < 0.05)
+    power.med[i] <- mean(p.med < p.val)
+    power.wmw[i] <- mean(p.wmw < p.val)
+    power.t[i] <- mean(p.t < p.val)
   }
   df_power <- data.frame(Distribution = dist_names,
                          med = rep(x = 0.0, length(distributions)),
@@ -114,6 +96,24 @@ for(delta in 0:1) {
   df_power$wmw <- power.wmw
   df_power$t <- power.t
   df_power <- melt(df_power, id.vars = 'Distribution')
+  return(df_power)
+}
+
+
+
+# Simulations
+N <- 20        # test simulations
+power.med <- power.wmw <- power.t <- c()
+
+for(n in c(20, 40)){
+# For simulations under H0 (delta = 0) and Ha (delta = 1)
+for(delta in 0:1) {
+  if(delta) {
+    title <- paste0('Power_', n, '_', N) }
+  else{
+    title <- paste0('TypeI_error_', n, '_', N) }
+    
+  df_power <- calc.rejection(N, n, delta)
   
   write.csv(df_power, paste(title, '.csv', sep=''))
   
@@ -128,4 +128,5 @@ for(delta in 0:1) {
           plot = p, 
           device = 'png'
           )
+}
 }
